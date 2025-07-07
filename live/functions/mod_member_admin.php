afd60e3f60e95e49580b8ab8b69c22c9d65f5b9b
dlgusrn <doqvmscjstk@naver.com>
2025-07-07 14:29:48 +0900

    관리자 - 관리자 관리 기능 오류 수정 1. 비밀번호 입력 안하고 저장하면 패스워드가 빈값으로 변경되는 문제 2. 관리자 관리, 성도 관리 값이 정상적으로 처리되지 않는 문제

--- live/functions/mod_member_admin.php
+++ live/functions/mod_member_admin.php
@@ -3,13 +3,16 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/live/inc/config.inc';
 
 // 비밀번호 체크
-if($_POST['password'] != $_POST['password_check']){
-    echo "<script>alert('비밀번호가 일치하지 않습니다.'); history.back();</script>" ;
-    exit ;
-}
+$password = '' ;
+if ( false == empty ( $_POST['password'] ) && true == isset ( $_POST['password'] ) && '' != $_POST['password'] ){
+    if ( $_POST['password'] != $_POST['password_check'] ) {
+        echo "<script>alert('비밀번호가 일치하지 않습니다.'); history.back();</script>" ;
+        exit ;
+    }
 
-// 비밀번호 암호화
-$password = hash ( "sha256", $_POST['password'] ) ;
+    // 비밀번호 암호화
+    $password = hash ( "sha256", $_POST['password'] ) ;
+}
 
 // 권한 세팅
 $live_manage = isset ( $_POST['live_manage'] ) ? 'Y' : 'N' ;   // 생방송 보기
@@ -21,26 +24,46 @@ $event_video = isset ( $_POST['event_video'] ) ? 'Y' : 'N' ;   // 다시보기 -
 $member_admin = isset ( $_POST['member_admin'] ) ? 'Y' : 'N' ; // 관리자 관리
 $member_saint = isset ( $_POST['member_saint'] ) ? 'Y' : 'N' ; // 성도 관리
 
-
 // DB 저장
-$DB->exec(
-    "UPDATE member_admin
-    SET
-        `username` = '$_POST[username]' ,
-        `password` = '$password' ,
-        `level` = '$_POST[level]' ,
-        `church` = '$_POST[church]' ,
-        `live_manage` = '$live_manage',
-        `live_list` = '$live_list' ,
-        `history` = '$history' ,
-        `black_list` = '$black_list' ,
-        `video_list` = '$video_list' ,
-        `event_video` = '$event_video' ,
-        `member_admin` = '$member_admin' ,
-        `member_saint` = '$member_saint' ,
-        `update_date` = NOW()
-    WHERE `idx` = $_GET[idx]
-" ) ;
+if ( '' != $password ) {
+    $DB->exec(
+        "UPDATE member_admin
+        SET
+            `username` = '$_POST[username]' ,
+            `password` = '$password' ,
+            `level` = '$_POST[level]' ,
+            `church` = '$_POST[church]' ,
+            `live_manage` = '$live_manage',
+            `live_list` = '$live_list' ,
+            `history` = '$history' ,
+            `black_list` = '$black_list' ,
+            `video_list` = '$video_list' ,
+            `event_video` = '$event_video' ,
+            `member_admin` = '$member_admin' ,
+            `member_saint` = '$member_saint' ,
+            `update_date` = NOW()
+        WHERE `idx` = $_GET[idx]
+    " ) ;
+} else {
+    $DB->exec(
+        "UPDATE member_admin
+        SET
+            `username` = '$_POST[username]' ,
+            `level` = '$_POST[level]' ,
+            `church` = '$_POST[church]' ,
+            `live_manage` = '$live_manage',
+            `live_list` = '$live_list' ,
+            `history` = '$history' ,
+            `black_list` = '$black_list' ,
+            `video_list` = '$video_list' ,
+            `event_video` = '$event_video' ,
+            `member_admin` = '$member_admin' ,
+            `member_saint` = '$member_saint' ,
+            `update_date` = NOW()
+        WHERE `idx` = $_GET[idx]
+    " ) ;
+}
+
 ?>
 <script>alert('관리자 정보가 수정 되었습니다.')</script>
 <meta http-equiv='refresh' content='0;url=<?=SITE_LIVE?>/admin/member/member_admin.html'>
